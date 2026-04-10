ac5576742f3a7f07f472552e289fae047932227d
Emillycomwy2000 <160077593+Emillycomwy2000@users.noreply.github.com>
2026-04-10 10:48:16 +0000

    tempoAtual

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-const botoes = document.querySelectorAl(".botao");
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
 
-for(let i = 0; i < botoes.length i = i + 1){
-    botoes[i]. onclick = function(){
+contadores[0].textContent = "Contagem regressiva!";
+
+for(let i = 0; i < botoes.length; i = i + 1){
+    botoes[i].onclick = function(){
         for(let j = 0; j < botoes.length; j = j + 1){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
- 
 }
+contadores[0].textContent = "Contagem regressiva!";
+tempoObjetivo01 = new Date(2026-10-05T00:00:00);
+lettempoAtual = new Date();
 console.log(botoes);
